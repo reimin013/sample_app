f635c915c5aedfc3017a1f0b26a652972cca32f4
reimi <utatatan.013@gmail.com>
2020-03-26 12:31:01 +0900

    [Add] 8章画像投稿機能で作成されたファイルを追加

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -2,38 +2,28 @@ class TodolistsController < ApplicationController
   def new
   	@list = List.new
   end
-
   def create
-  	# ストロングパラメータを使用
   	list = List.new(list_params)
-  	# DBへ保存する
   	list.save
-
-  	redirect_to todolist_path(list.id) #詳細画面へリダイレクト
+  	redirect_to todolist_path(list.id)
   end
-
   def index
-      @lists = List.all
+    @lists = List.all
   end
-
   def show
-      @list = List.find(params[:id])
-  end
-
+    @list = List.find(params[:id])
+   end
   def edit
-      @list = List.find(params[:id])
+    @list = List.find(params[:id])
+   end
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to todolist_path(list.id)
   end
 
-  def upadate
-      list = List.find(params[:id])
-      list.update(list_params)
-      redirect_to todolist_path(list.id)
-  end
-
-  private
-
+   private
   def list_params
-  	params.require(:list).permit(:title, :body)
+  	params.require(:list).permit(:title, :body, :image)
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,8 @@ Rails.application.routes.draw do
 
   get 'todolists/:id' => 'todolists#show', as: 'todolist'
 
-  get 'todolosts/:id/edit' => 'todolists#edit', as: 'edit_todolist'
+  get 'todolists/:id/edit' => 'todolists#edit', as: 'edit_todolist'
 
-  patch 'todolists/:id' => 'rodolists#update', as: 'update_todolist'
+  patch 'todolists/:id' => 'todolists#update', as: 'update_todolist'
 
 end
